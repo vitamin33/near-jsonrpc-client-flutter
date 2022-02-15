2c27d8c3c375c12d4bd7e790b61ca96b1aa7756a
vitaliiserbyn <vivitseser@gmail.com>
2022-02-16 00:49:58 +0200

    Add headers for JsonRpcClient;

--- lib/struct/json_rpc_client.dart
+++ lib/struct/json_rpc_client.dart
@@ -1,14 +1,20 @@
+import 'dart:collection';
 import 'dart:io';
 
 import 'package:http/http.dart' as http;
 
+const String contentType = "content-type";
+const String appJsonContentType = "application/json";
+
 class JsonRpcClient {
 
   final String serverUrl;
+  final Map<String, String> headers;
 
-  JsonRpcClient(this.serverUrl);
+  JsonRpcClient(this.serverUrl, this.headers);
 
   JsonRpcClient connect(String serverUrl) {
-    return JsonRpcClient(serverUrl);
+    Map<String, String> headers = {contentType : appJsonContentType};
+    return JsonRpcClient(serverUrl, headers);
   }
 }
