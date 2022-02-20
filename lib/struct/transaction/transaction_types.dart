35e2ffcc4604deb8accb317f0cc7bd2df31043f7
vitaliiserbyn <vivitseser@gmail.com>
2022-02-20 21:28:19 +0200

    Add action type implementation methods;

--- lib/struct/transaction/transaction_types.dart
+++ lib/struct/transaction/transaction_types.dart
@@ -13,7 +13,44 @@ class IAction {
   }
 
   static IAction deployContract(Uint8List code) {
-  return DeployContract(Action.deployContract, code);
+    return DeployContract(Action.deployContract, code);
+  }
+
+  static stringifyJsonOrBytes(Uint8List args) {
+    //TODO add ability to pass objects as arguments
+    return args;
+  }
+
+  // Constructs {@link Action} instance representing contract method call.
+  // @param methodName the name of the method to call
+  // @param args arguments to pass to method. Can be either plain JS object which gets serialized as JSON automatically
+  //  or `Uint8Array` instance which represents bytes passed as is.
+  // @param gas max amount of gas that method call can use
+  // @param deposit amount of NEAR (in yoctoNEAR) to send together with the call
+  // @param stringify Convert input arguments into bytes array.
+  static IAction functionCall(
+      String methodName, Uint8List args, BigInt gas, BigInt deposit) {
+    return FunctionCall(Action.functionCall, methodName, args, gas, deposit);
+  }
+
+  static IAction transfer(BigInt deposit) {
+    return Transfer(Action.transfer, deposit);
+  }
+
+  static IAction stake(BigInt stake, PublicKey publicKey) {
+    return Stake(Action.stake, stake, publicKey);
+  }
+
+  static IAction addKey(PublicKey publicKey, AccessKey accessKey) {
+    return AddKey(Action.addKey, publicKey, accessKey);
+  }
+
+  static IAction deleteKey(PublicKey publicKey) {
+    return DeleteKey(Action.deleteKey, publicKey);
+  }
+
+  static IAction deleteAccount(String beneficiaryId) {
+    return DeleteAccount(Action.deleteAccount, beneficiaryId);
   }
 }
 
@@ -26,14 +63,16 @@ class DeployContract extends IAction {
 
   DeployContract(Action action, this.code) : super(action);
 }
+
 class FunctionCall extends IAction {
   String methodName;
   Uint8List args;
   BigInt gas;
   BigInt deposit;
 
-  FunctionCall(Action action, this.methodName, this.args, this.gas, this.deposit)
-  : super(action);
+  FunctionCall(
+      Action action, this.methodName, this.args, this.gas, this.deposit)
+      : super(action);
 }
 
 class Transfer extends IAction {
@@ -67,4 +106,3 @@ class DeleteAccount extends IAction {
 
   DeleteAccount(Action action, this.beneficiaryId) : super(action);
 }
-
